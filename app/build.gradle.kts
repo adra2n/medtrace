224dc6d7995c211f79b3ef21ebe675f6261a550e
gaohe <gaoheby@gmail.com>
2026-07-16 18:38:11 +0800

    bump version to v1.2.0 (code 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yy.chiyaole"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "v1.1.0"
+        versionCode = 5
+        versionName = "v1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
