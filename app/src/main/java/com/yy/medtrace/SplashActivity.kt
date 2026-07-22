1b35f10eef3b90caf7270259fb5ba15c8ebc8738
gaohe <gaoheby@gmail.com>
2026-07-22 15:36:18 +0800

    fix: use Activity instead of AppCompatActivity for SplashActivity

--- app/src/main/java/com/yy/medtrace/SplashActivity.kt
+++ app/src/main/java/com/yy/medtrace/SplashActivity.kt
@@ -1,13 +1,13 @@
 package com.yy.medtrace
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import com.umeng.analytics.MobclickAgent
 import com.umeng.commonsdk.UMConfigure
 import com.yy.medtrace.data.settings.OnboardingStore
@@ -15,18 +15,16 @@ import com.yy.medtrace.data.settings.PrivacyConsentStore
 import kotlinx.coroutines.runBlocking
 
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : Activity() {
 
     private var splashContainer: FrameLayout? = null
     private var skipTextView: TextView? = null
     private var countDownTimer: CountDownTimer? = null
-    private var adLoaded = false
 
     companion object {
         private const val SKIP_DELAY = 5000L
         private const val COUNT_DOWN_INTERVAL = 1000L
         private const val UMENG_APPKEY = "6a5b46a5cbfa6959517c8588"
-        private const val AD_UNIT_ID = "100012523"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,9 +48,7 @@ class SplashActivity : AppCompatActivity() {
         UMConfigure.init(this, UMENG_APPKEY, "official", UMConfigure.DEVICE_TYPE_PHONE, null)
         MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO)
 
-        // 加载开屏广告
-        // U-AppWin SDK 会在后台加载广告，加载完成后回调
-        // 这里我们先启动倒计时，广告加载完成后会自动展示
+        // 启动倒计时
         startCountDown()
     }
 
